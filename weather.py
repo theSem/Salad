510848ae757769d76e1dba8616ef9aea03f0e323
Sam M <maten009@umn.edu>
2019-02-03 12:58:48 -0600

    Update weather.py

--- weather.py
+++ weather.py
@@ -8,10 +8,10 @@ AERIS_SECRET = 'Ergc2GajB2wYwdiLMd4ily0sBknJVgSZDZ7NngGE'
 def weatherUpdate(POSTAL_CODE):
     weatherbit = requests.get("https://api.weatherbit.io/v2.0/forecast/daily?postal_code={0}&key={1}".format(POSTAL_CODE, WEATHERBIT_KEY))
     bitData = weatherbit.json()['data']
-    weather = ""
+    weatherString = ""
     for day, weather in enumerate(bitData):
-        weather += "{0}: {1}\n".format(day + 1, bitData['weather']['description'])
-    return weather
+        weatherString += "{0}: {1}\n".format(day + 1, weather['weather']['description'])
+    return weatherString
 
 def advisoryUpdate(POSTAL_CODE):
     aerisweather = requests.get("https://api.aerisapi.com/alerts/{0}?&format=json&limit=10&client_id={1}&client_secret={2}".format(POSTAL_CODE, AERIS_ID, AERIS_SECRET))
