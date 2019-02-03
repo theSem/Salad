ed3f016785e9fe600742329ba1ad0386415a1429
Lukas <lukas@argo.acm.umn.edu>
2019-02-03 07:34:29 -0600

    set up page for contributing data

--- routes.py
+++
@@ -1,5 +0,0 @@
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Ashmita'}
-    return render_template('index.html', title='Home', user=user)
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # /usr/bin/env python
 # Download the twilio-python library from twilio.com/docs/libraries/python
-from flask import Flask, request
+from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 import pyrebase 
 
@@ -19,8 +19,6 @@ db = firebase.database()
 
 app = Flask(__name__)
 
-commands = {"sub": subscribe(number), "unsub": unsubscribe(number), "set phonenumber": "set phonenumber", "set location": "set location"}
-
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
@@ -29,6 +27,7 @@ def sms_ahoy_reply():
     body = request.values.get('Body', None)
     number = request.values.get('From', None)
     print("response was: ", body)
+    commands = {"sub": subscribe(number,resp), "unsub": unsubscribe(number,resp)}
     print("\nresponse was from: ", number)
     if body in commands:
         return process(commands[body], number)
@@ -52,17 +51,23 @@ def process(command, number):     #this function takes in valid commands and doe
         resp.message("no action required for command(valid)")
     return str(resp)
 
-def subscribe(number):
+def subscribe(number,resp):
     db.child("numbers").update({str(number): True})
     resp.message("You have successfully subscribed")
 
-def unsubscribe(number):
+def unsubscribe(number,resp):
     db.child("numbers").child(str(number)).remove()
     resp.message("You have successfully unsubscribed")
 
-def set_location(location):
+def set_location(location,resp):
     db.child("numbers").child(str(location)).remove()
     resp.message("You have successfully set the location")
+
+@app.route("/contribute")
+def index():
+    user = {'username':'Person'}
+    return render_template('index.html', title='Home', user=user)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
