89ccc548bf3e66bb788e515f8a07c29e5b9ff629
Sam M <maten009@umn.edu>
2019-02-02 22:05:33 -0600

    fuck lukas

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # /usr/bin/env python
 # Download the twilio-python library from twilio.com/docs/libraries/python
-from flask import Flask, request, redirect
-from twilio import twiml
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
     # Start our response
-    resp = twiml.Response()
+    resp = MessagingResponse()
 
     # Add a message
     resp.message("Ahoy! Thanks so much for your message.")
