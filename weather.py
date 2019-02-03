1e47759e3fa17e4273e50b29ce2da8e2a2227b6f
Sam M <maten009@umn.edu>
2019-02-03 12:55:35 -0600

    Weather return

--- weather.py
+++ weather.py
@@ -9,17 +9,19 @@ AERIS_SECRET = 'Ergc2GajB2wYwdiLMd4ily0sBknJVgSZDZ7NngGE'
 def weatherUpdate():
     weatherbit = requests.get("https://api.weatherbit.io/v2.0/forecast/daily?postal_code={0}&key={1}".format(POSTAL_CODE, WEATHERBIT_KEY))
     bitData = weatherbit.json()['data']
+    weather = ""
     for day, weather in enumerate(bitData):
-        print(day + 1, ": ", weather['weather']['description'])
+        weather += "{0}: {1}\n".format(day + 1, weather['weather']['description'])
+    return weather
 
 def advisoryUpdate():
     aerisweather = requests.get("https://api.aerisapi.com/alerts/{0}?&format=json&limit=10&client_id={1}&client_secret={2}".format(POSTAL_CODE, AERIS_ID, AERIS_SECRET))
     aerisData = aerisweather.json()['response']
 
     try:
-        print(aerisData[0]['details']['body'])
+        return aerisData[0]['details']['body']
     except:
-        print("No current weather advisories.")
+        return "No current weather advisories."
 
 
 def droughtUpdate():
@@ -29,7 +31,6 @@ def droughtUpdate():
     try:
         droughtStuff = droughtData['response'][0]['details']
         droughtStatus = droughtStuff['risk']['name']
-        print(droughtStatus)
-        print("Issued until: ", datetime.utcfromtimestamp(droughtStuff['range']['maxTimestamp']).strftime('%Y-%m-%d %H:%M:%S'))
+        return droughtStatus, "\nIssued until: ", datetime.utcfromtimestamp(droughtStuff['range']['maxTimestamp']).strftime('%Y-%m-%d %H:%M:%S')
     except:
-        print("No current drought risk.")
+        return "No current drought risk."
